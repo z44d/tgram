2151d9cf0c64235ee9e663a25226636896fed393
Zaid _ <vsszaid@icloud.com>
2024-06-15 16:25:31 +0300

    refactor

--- tgram/__init__.py
+++ tgram/__init__.py
@@ -1,6 +1,12 @@
 from .methods import Methods
 from .decorators import Decorators
 
+from . import types
+
+__all__ = [
+    "types",
+    "TgBot"
+]
 
 class TgBot(Methods, Decorators):
     pass  # TODO
