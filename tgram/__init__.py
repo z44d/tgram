6e141e83082b9a970e0f762a55db2cba867a2d67
Zaid _ <vsszaid@icloud.com>
2024-06-26 17:12:57 +0300

    move to utils.py

--- tgram/__init__.py
+++ tgram/__init__.py
@@ -1,10 +1,4 @@
-__all__ = ["types", "TgBot", "handlers", "filters", "API_URL", "ALL_UPDATES"]
+__all__ = ["types", "TgBot", "handlers", "filters"]
 
 from . import types, handlers, filters
 from .tgbot import TgBot
-
-API_URL = "https://api.telegram.org/"
-ALL_UPDATES = [
-    getattr(handlers.Handlers, i)
-    for i in filter(lambda x: not x.startswith("_"), handlers.Handlers.__dict__)
-]
