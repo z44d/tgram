a9dca25b7b9cc73c217aa3d0da130ce8033f5821
Zaid <me@zaid.pro>
2024-06-29 19:04:04 +0300

    add bound method for Message (#10)
    * test bound method on Message
    
    * add sync wraper for bound method

---
+++ tgram/bound.py
@@ -0,0 +1,51 @@
+import tgram
+import inspect
+import sys
+
+from typing import Union, List
+
+from tgram import sync
+
+
+class MessageB:
+    async def reply_text(
+        self: "tgram.types.Message",
+        text: str,
+        parse_mode: str = None,
+        entities: List["tgram.types.MessageEntity"] = None,
+        link_preview_options: "tgram.types.LinkPreviewOptions" = None,
+        disable_notification: bool = None,
+        protect_content: bool = None,
+        message_effect_id: str = None,
+        reply_markup: Union[
+            "tgram.types.InlineKeyboardMarkup",
+            "tgram.types.ReplyKeyboardMarkup",
+            "tgram.types.ReplyKeyboardRemove",
+            "tgram.types.ForceReply",
+        ] = None,
+    ) -> "tgram.types.Message":
+        return await self._me.send_message(
+            chat_id=self.chat.id,
+            text=text,
+            business_connection_id=self.business_connection_id,
+            message_thread_id=self.message_thread_id,
+            parse_mode=parse_mode if parse_mode is not None else self._me.parse_mode,
+            entities=entities,
+            link_preview_options=link_preview_options or self._me.link_preview_options,
+            disable_notification=disable_notification,
+            protect_content=protect_content
+            if protect_content is not None
+            else self._me.protect_content,
+            message_effect_id=message_effect_id,
+            reply_markup=reply_markup,
+            reply_parameters=tgram.types.ReplyParameters(self.message_id),
+        )
+
+    @property
+    def id(self: "tgram.types.Message") -> int:
+        return self.message_id
+
+
+for name, obj in inspect.getmembers(sys.modules[__name__]):
+    if inspect.isclass(obj):
+        sync.wrap(obj)
