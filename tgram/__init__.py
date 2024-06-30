6ae8bfd741dcfaa63745f15eed78ef4d52509a41
ihatemyself0 <173957402+ihatemyself0@users.noreply.github.com>
2024-06-30 06:46:01 +0300

    add __version__, author name, license and copyright to __init__ (#12)

--- tgram/__init__.py
+++ tgram/__init__.py
@@ -1,4 +1,9 @@
 __all__ = ["types", "TgBot", "handlers", "filters"]
+__version__ = "1.0.9"
+
+__author__ = "Zaid"
+__copyright__ = "Copyright (C) 2024-present Zaid <https://github.com/2ei>"
+__license__ = "MIT"
 
 from . import types, handlers, filters
 from .tgbot import TgBot
