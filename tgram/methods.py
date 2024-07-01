468298196af3d0f26849b8415f46f0273624d758
Zaid <me@zaid.pro>
2024-07-02 00:16:17 +0300

    New method: send_paid_media

--- tgram/methods.py
+++ tgram/methods.py
@@ -48,6 +48,7 @@ from .types import (
     StarTransactions,
     PassportElementError,
     GameHighScore,
+    InputPaidMedia,
 )
 
 
@@ -573,6 +574,41 @@ class TelegramBotMethods:
             if protect_content is not None
             else self.protect_content,
             message_effect_id=message_effect_id,
+            reply_parameters=reply_parameters,
+            reply_markup=reply_markup,
+        )
+        return Message._parse(me=self, d=result["result"])
+
+    async def send_paid_media(
+        self: "tgram.TgBot",
+        chat_id: Union[int, str],
+        star_count: int,
+        media: List[InputMedia],
+        caption: str = None,
+        parse_mode: str = None,
+        caption_entities: List[MessageEntity] = None,
+        show_caption_above_media: bool = None,
+        disable_notification: bool = None,
+        protect_content: bool = None,
+        reply_parameters: ReplyParameters = None,
+        reply_markup: Union[
+            InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply
+        ] = None,
+    ) -> Message:
+        """https://core.telegram.org/bots/api#sendpaidmedia"""
+        result = await self._send_request(
+            "sendPaidMedia",
+            chat_id=chat_id,
+            star_count=star_count,
+            media=media,
+            caption=caption,
+            parse_mode=parse_mode or self.parse_mode,
+            caption_entities=caption_entities,
+            show_caption_above_media=show_caption_above_media,
+            disable_notification=disable_notification,
+            protect_content=protect_content
+            if protect_content is not None
+            else self.protect_content,
             reply_parameters=reply_parameters,
             reply_markup=reply_markup,
         )
