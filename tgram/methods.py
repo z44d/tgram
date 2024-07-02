3abf6539d71a7534f7884493657e463510d815c2
Zaid <me@zaid.pro>
2024-07-02 23:49:37 +0300

    Add download_file method

--- tgram/methods.py
+++ tgram/methods.py
@@ -1,6 +1,7 @@
 # This is auto generated file, if you found any issue, please report me here: https://github.com/2ei/tgram/issues/new
+import io
 
-from typing import List, Union
+from typing import List, Union, BinaryIO
 import tgram
 from .types import (
     Update,
@@ -50,6 +51,10 @@ from .types import (
     GameHighScore,
     InputPaidMedia,
 )
+
+from .errors import APIException
+
+from pathlib import Path
 
 
 class TelegramBotMethods:
@@ -905,6 +910,26 @@ class TelegramBotMethods:
             file_id=file_id,
         )
         return File._parse(me=self, d=result["result"])
+
+    async def download_file(
+        self: "tgram.TgBot", file_id: str, file_path: str = None, in_memory: bool = None
+    ) -> Path | BinaryIO:
+        file = await self.get_file(file_id)
+        file_path = file_path or file.file_path
+        url = self.api_url + f"file/bot{self.bot_token}/{file.file_path}"
+        session = await self._get_session()
+        async with session.request("GET", url=url) as response:
+            if response.status != 200:
+                raise APIException("Download file", response)
+        result = await response.read()
+        if in_memory:
+            memory_file = io.BytesIO()
+            memory_file.write(result)
+            memory_file.name = file_path
+        else:
+            with open(Path(file_path), "wb") as f:
+                f.write(result)
+            return Path(file_path)
 
     async def ban_chat_member(
         self: "tgram.TgBot",
