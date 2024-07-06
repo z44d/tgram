483e5ab33a616143f19bedc98419e0abf9fe9fbb
Zaid <me@zaid.pro>
2024-07-07 01:02:56 +0300

    add CallbackQuery listener example

---
+++ examples/callback_listener.py
@@ -0,0 +1,34 @@
+from tgram import TgBot, filters
+
+from tgram.types import (
+    Message,
+    CallbackQuery,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
+from tgram.handlers import Handlers
+
+bot = TgBot("API_TOKEN_HERE")
+
+
+@bot.on_message(filters.private & filters.command("start"))
+async def on_message(bot: TgBot, m: Message):
+    await m.reply_text(
+        "Click on the button.",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Button", callback_data="data")]]
+        ),
+    )
+    # This is will make it work only one time.
+    return await bot.ask(
+        update_type=Handlers.CALLBACK_QUERY,
+        next_step=answer_callback,
+        filters=filters.user(m.from_user.id) & filters.regex("^data$"),
+    )
+
+
+async def answer_callback(_, c: CallbackQuery, __):
+    return await c.answer("Hi, how are you?", show_alert=True)
+
+
+bot.run_for_updates()
