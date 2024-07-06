236f094abcf6d80bc9b4864a9bbc7ba5e60f07fe
Zaid <me@zaid.pro>
2024-07-07 02:26:01 +0300

    new method: send_media_from_file_id

--- tgram/methods.py
+++ tgram/methods.py
@@ -2387,3 +2387,42 @@ class TelegramBotMethods:
                 filters=filters or tgram.filters.all,
             )
         )
+
+    async def send_media_from_file_id(
+        self: "tgram.TgBot",
+        chat_id: Union[int, str],
+        file_id: str,
+        business_connection_id: str = None,
+        message_thread_id: int = None,
+        caption: str = None,
+        parse_mode: str = None,
+        caption_entities: List[MessageEntity] = None,
+        show_caption_above_media: bool = None,
+        disable_notification: bool = None,
+        protect_content: bool = None,
+        message_effect_id: str = None,
+        reply_parameters: ReplyParameters = None,
+        reply_markup: Union[
+            InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply
+        ] = None,
+    ) -> Message:
+        file = await self.get_file(file_id)
+        file_type = file.file_path.split("/")[0][:-1].title()
+
+        result = await self._send_request(
+            f"send{file_type}",
+            chat_id=chat_id,
+            business_connection_id=business_connection_id,
+            message_thread_id=message_thread_id,
+            caption=caption,
+            parse_mode=parse_mode,
+            caption_entities=caption_entities,
+            show_caption_above_media=show_caption_above_media,
+            disable_notification=disable_notification,
+            protect_content=protect_content,
+            message_effect_id=message_effect_id,
+            reply_parameters=reply_parameters,
+            reply_markup=reply_markup,
+        )
+
+        return Message._parse(me=self, d=result["result"])
