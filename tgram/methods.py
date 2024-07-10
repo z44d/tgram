1f02e9faa85d83ad516439904626c8b8f0990d60
Mr. Mahdi <alidiaa0014@gmail.com>
2024-07-10 05:20:59 +0300

    fix download_file (#25)
    * Update methods.py
    
    ---------
    
    Co-authored-by: Zaid <me@zaid.pro>

--- tgram/methods.py
+++ tgram/methods.py
@@ -920,13 +920,13 @@ class TelegramBotMethods:
         self: "tgram.TgBot", file_id: str, file_path: str = None, in_memory: bool = None
     ) -> Union[Path, io.BytesIO]:
         file = await self.get_file(file_id)
-        file_path = file_path or file.file_path
+        file_path = file_path or file.file_path.split("/")[1]
         url = self.api_url + f"file/bot{self.bot_token}/{file.file_path}"
         session = await self._get_session()
         async with session.request("GET", url=url) as response:
             if response.status != 200:
                 raise APIException("Download file", response)
-        result = await response.read()
+            result = await response.read()
         if in_memory:
             memory_file = io.BytesIO()
             memory_file.write(result)
