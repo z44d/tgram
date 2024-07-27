775cd25fb1953b8d2512adcb59880db7a0281971
Zaid <me@zaid.pro>
2024-07-27 23:33:13 +0300

    improve edit_message_media

--- tgram/methods/edit_message_media.py
+++ tgram/methods/edit_message_media.py
@@ -4,6 +4,8 @@ from tgram.types import InlineKeyboardMarkup
 from tgram.types import InputMedia
 from tgram.types import Message
 
+from tgram.utils import convert_input_media
+
 
 class EditMessageMedia:
     async def edit_message_media(
@@ -15,14 +17,16 @@ class EditMessageMedia:
         inline_message_id: str = None,
         reply_markup: InlineKeyboardMarkup = None,
     ) -> Union[Message, bool]:
+        converted, file = convert_input_media([media])
         result = await self._send_request(
             "editMessageMedia",
-            media=media,
+            media=converted[0],
             business_connection_id=business_connection_id,
             chat_id=chat_id,
             message_id=message_id,
             inline_message_id=inline_message_id,
             reply_markup=reply_markup,
+            **file,
         )
         return (
             Message._parse(me=self, d=result["result"])
