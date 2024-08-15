df89e7f0fbb194680be67272e5c730ebcd7f2b10
Zaid <me@zaid.pro>
2024-08-15 20:43:18 +0300

    add the field until_date

--- tgram/types/_chat_member_member.py
+++ tgram/types/_chat_member_member.py
@@ -16,6 +16,9 @@ class ChatMemberMember(Type_):
     :param user: Information about the user
     :type user: :class:`tgram.types.User`
 
+    :param until_date: Optional. Date when the user's subscription will expire; Unix time
+    :type status: :obj:`int`
+
     :return: Instance of the class
     :rtype: :class:`tgram.types.ChatMemberMember`
     """
@@ -24,12 +27,14 @@ class ChatMemberMember(Type_):
         self,
         status: "str" = None,
         user: "tgram.types.User" = None,
+        until_date: "int" = None,
         me: "tgram.TgBot" = None,
         json: "dict" = None,
     ):
         super().__init__(me=me, json=json)
         self.status = status
         self.user = user
+        self.until_date = until_date
 
     @staticmethod
     def _parse(
@@ -41,6 +46,7 @@ class ChatMemberMember(Type_):
                 json=d,
                 status=d.get("status"),
                 user=tgram.types.User._parse(me=me, d=d.get("user")),
+                until_date=d.get("until_date"),
             )
             if d and (force or me and __class__.__name__ not in me._custom_types)
             else None
