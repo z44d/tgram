cde64b70c55d9d075f7d534203cb1d7766380410
Zaid <me@zaid.pro>
2024-08-21 21:25:12 +0300

    Update Tgram to v1.10.9

--- tgram/__init__.py
+++ tgram/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ["types", "TgBot", "handlers", "filters", "compose", "StopPropagation"]
-__version__ = "1.10.8"
+__version__ = "1.10.9"
 
 __author__ = "Zaid"
 __copyright__ = "Copyright (C) 2024-present Zaid <https://github.com/z44d>"
