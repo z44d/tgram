771f3951b8041620c79cd76d1979d1beec485887
Zaid <me@zaid.pro>
2024-09-06 17:43:59 +0300

    fix GiveawayCreated type

--- tgram/types/_giveaway_created.py
+++ tgram/types/_giveaway_created.py
@@ -1,58 +1,37 @@
 import tgram
 from .type_ import Type_
 
-from typing import List, Optional
+from typing import Optional
 
 
 class GiveawayCreated(Type_):
     """
-    This object represents a service message about the creation of a scheduled giveaway. Currently holds no information.
+    This object represents a service message about the creation of a scheduled giveaway.
+
+    Telegram documentation: https://core.telegram.org/bots/api#giveawaycreated
+
+    :param prize_star_count: Optional. The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
+    :type prize_star_count: :obj:`int`
+
+    :return: Instance of the class
+    :rtype: :class:`GiveawayCreated`
     """
 
     def __init__(
         self,
-        chats: List["tgram.types.Chat"] = None,
-        winners_selection_date: "int" = None,
-        winner_count: "int" = None,
-        only_new_members: "bool" = None,
-        has_public_winners: "bool" = None,
-        prize_description: "str" = None,
-        country_codes: List["str"] = None,
-        premium_subscription_month_count: "int" = None,
+        prize_star_count: int = None,
         me: "tgram.TgBot" = None,
         json: "dict" = None,
     ):
         super().__init__(me=me, json=json)
-        self.chats = chats
-        self.winners_selection_date = winners_selection_date
-        self.winner_count = winner_count
-        self.only_new_members = only_new_members
-        self.has_public_winners = has_public_winners
-        self.prize_description = prize_description
-        self.country_codes = country_codes
-        self.premium_subscription_month_count = premium_subscription_month_count
+        self.prize_star_count = prize_star_count
 
     @staticmethod
     def _parse(
         me: "tgram.TgBot" = None, d: dict = None, force: bool = None
     ) -> Optional["tgram.types.GiveawayCreated"]:
         return (
-            GiveawayCreated(
-                me=me,
-                json=d,
-                chats=[tgram.types.Chat._parse(me=me, d=i) for i in d.get("chats")]
-                if d.get("chats")
-                else None,
-                winners_selection_date=d.get("winners_selection_date"),
-                winner_count=d.get("winner_count"),
-                only_new_members=d.get("only_new_members"),
-                has_public_winners=d.get("has_public_winners"),
-                prize_description=d.get("prize_description"),
-                country_codes=d.get("country_codes"),
-                premium_subscription_month_count=d.get(
-                    "premium_subscription_month_count"
-                ),
-            )
+            GiveawayCreated(me=me, json=d, prize_star_count=d.get("prize_star_count"))
             if d and (force or me and __class__.__name__ not in me._custom_types)
             else None
             if not d
