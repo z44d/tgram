18b0e1c30aafb89266d254f3ca1350dfa489676e
Aws / <151061059+IceBytes@users.noreply.github.com>
2024-09-17 23:03:17 +0300

    Syntax highlight

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
 import os
 import sys
 
@@ -15,17 +16,17 @@ copyright = "2024, Zaid"
 author = "Zaid"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_permalinks_icon = '<span>#</span>'
 html_theme = 'sphinxawesome_theme'
 html_static_path = ["_static"]
+
+pygments_style = "friendly"  
+pygments_style_dark = "monokai"  
