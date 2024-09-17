1ec9b77b0840d4a42eaed5c98ac3cc78e72b0ef1
Aws / <151061059+IceBytes@users.noreply.github.com>
2024-09-17 22:35:49 +0300

    Change html theme: sphinx awesome theme

--- docs/conf.py
+++ docs/conf.py
@@ -26,5 +26,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_permalinks_icon = '<span>#</span>'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ["_static"]
