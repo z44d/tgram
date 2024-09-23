d4ffa3f53bfb484b45b1480536d753ba5e22126b
Aws / <151061059+IceBytes@users.noreply.github.com>
2024-09-23 21:46:10 +0300

    Change theme

--- docs/conf.py
+++ docs/conf.py
@@ -26,5 +26,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "pytorch_sphinx_theme"
 html_static_path = ["_static"]
