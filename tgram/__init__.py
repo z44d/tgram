cfa3172df19ddb9f34439430b026642e9d207f5d
Zaid <me@zaid.pro>
2024-10-14 21:42:58 +0300

    update __init__

--- tgram/__init__.py
+++ tgram/__init__.py
@@ -1,11 +1,6 @@
 __all__ = ["types", "TgBot", "handlers", "filters", "compose", "StopPropagation"]
 __version__ = "1.11.7"
 
-__author__ = "Zaid"
-__copyright__ = "Copyright (C) 2024-present Zaid <https://github.com/z44d>"
-__license__ = "MIT"
-
-from . import types, handlers, filters
 from .client import TgBot
 from .sync import compose
 
