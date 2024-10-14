186b2406721f989fbe908fb1c6fb0131d2530357
Zaid <me@zaid.pro>
2024-10-14 21:42:17 +0300

    fix

--- tgram/utils/parse_mode.py
+++ tgram/utils/parse_mode.py
@@ -6,8 +6,8 @@ from typing import Optional
 def get_parse_mode(
     bot: Optional["tgram.TgBot"] = None, parse_mode: Optional[str] = None
 ) -> Optional[str]:
-    return (
-        None
-        if parse_mode and parse_mode.lower() in ("d", "disable", "disabled")
-        else parse_mode or (bot.parse_mode if bot else None)
-    )
+    parse_mode = parse_mode or (bot.parse_mode if bot else None)
+
+    if parse_mode and parse_mode.lower() in {"d", "disable", "disabled"}:
+        return None
+    return parse_mode
