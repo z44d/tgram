8f547436da6557cb74d7127eba3cf99629283f02
Zaid <me@zaid.pro>
2024-10-14 21:49:51 +0300

    Update Tgram to v1.11.8

--- tgram/__init__.py
+++ tgram/__init__.py
@@ -1,5 +1,16 @@
-__all__ = ["types", "TgBot", "handlers", "filters", "compose", "StopPropagation"]
-__version__ = "1.11.7"
+__all__ = [
+    "types",
+    "TgBot",
+    "handlers",
+    "filters",
+    "compose",
+    "StopPropagation",
+    "utils",
+    "storage",
+]
+__version__ = "1.11.8"
+
+from . import filters, utils, storage, handlers, types
 
 from .client import TgBot
 from .sync import compose
