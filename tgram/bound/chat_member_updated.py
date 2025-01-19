10c6d9ec0baa25b14b7f4513bc269b0555b78ac3
Zaid <me@zaid.pro>
2025-01-19 21:45:08 +0200

    chore: :memo: Add docstring for user property in ChatMemberUpdatedB

--- tgram/bound/chat_member_updated.py
+++ tgram/bound/chat_member_updated.py
@@ -6,6 +6,12 @@ from typing import Optional
 class ChatMemberUpdatedB:
     @property
     def user(self: "tgram.types.ChatMemberUpdated") -> Optional["tgram.types.User"]:
+        """
+        Get the user who sent the callback query.
+
+        Returns:
+            Optional[tgram.types.User]: The user who sent the callback query.
+        """
         return self.from_user
 
     sender_user = user
