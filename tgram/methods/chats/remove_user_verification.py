0f973b2593eb20fd0377a3f9b5f77c0dfca21c26
Zaid <me@zaid.pro>
2025-01-23 20:56:11 +0200

    feat: add remove_user_verification method

---
+++ tgram/methods/chats/remove_user_verification.py
@@ -0,0 +1,23 @@
+import tgram
+
+class RemoveUserVerification:
+    async def remove_user_verification(
+        self: "tgram.TgBot",
+        user_id: int,
+    ) -> bool:
+        """
+        Removes verification from a user who is currently verified on behalf of the organization represented by the bot. Returns True on success.
+        Telegram documentation: https://core.telegram.org/bots/api#removeuserverification
+
+        :param user_id: Unique identifier of the target user
+        :type user_id: :obj:`int`
+
+        :return: True on success
+        :rtype: :obj:`bool`
+        """
+
+        result = await self(
+            "removeUserVerification",
+            user_id=user_id,
+        )
+        return result["result"]
