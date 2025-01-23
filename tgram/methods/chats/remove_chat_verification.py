6c73b74a09d3cd679e5faca249177a6ee23e2e9e
Zaid <me@zaid.pro>
2025-01-23 20:55:47 +0200

    feat: add remove_chat_verification method

---
+++ tgram/methods/chats/remove_chat_verification.py
@@ -0,0 +1,25 @@
+import tgram
+
+from typing import Union
+
+class RemoveChatVerification:
+    async def remove_chat_verification(
+        self: "tgram.TgBot",
+        chat_id: Union[str, int],
+    ) -> bool:
+        """
+        Removes verification from a chat that is currently verified on behalf of the organization represented by the bot. Returns True on success.
+        Telegram documentation: https://core.telegram.org/bots/api#removechatverification
+
+        :param chat_id: Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+        :type chat_id: :obj:`int` or :obj:`str`
+
+        :return: True on success
+        :rtype: :obj:`bool`
+        """
+
+        result = await self(
+            "removeChatVerification",
+            chat_id=chat_id,
+        )
+        return result["result"]
