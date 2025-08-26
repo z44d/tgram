70f4a353786b6b8a79d7b88ea6ba9a9d59e1c11e
Za¡d <vsszaid@icloud.com>
2025-08-26 23:15:38 +0300

    feat: Added the field checklist_task_id to the class ReplyParameters, allowing bots to reply to a specific checklist task.

--- tgram/types/_reply_parameters.py
+++ tgram/types/_reply_parameters.py
@@ -3,7 +3,6 @@ from .type_ import Type_
 
 from typing import List, Union, Optional
 
-
 class ReplyParameters(Type_):
     """
     Describes reply parameters for the message that is being sent.
@@ -13,10 +12,10 @@ class ReplyParameters(Type_):
     :param message_id: Identifier of the message that will be replied to in the current chat, or in the chat chat_id if it is specified
     :type message_id: :obj:`int`
 
-    :param chat_id: Optional. If the message to be replied to is from a different chat, unique identifier for the chat or username of the channel (in the format @channelusername)
+    :param chat_id: Optional. If the message to be replied to is from a different chat, unique identifier for the chat or username of the channel (in the format @channelusername). Not supported for messages sent on behalf of a business account and messages from channel direct messages chats.
     :type chat_id: :obj:`int` or :obj:`str`
 
-    :param allow_sending_without_reply: Optional. Pass True if the message should be sent even if the specified message to be replied to is not found; can be used only for replies in the same chat and forum topic.
+    :param allow_sending_without_reply: Optional. Pass True if the message should be sent even if the specified message to be replied to is not found. Always False for replies in another chat or forum topic. Always True for messages sent on behalf of a business account.
     :type allow_sending_without_reply: :obj:`bool`
 
     :param quote: Optional. Quoted part of the message to be replied to; 0-1024 characters after entities parsing. The quote must be an exact substring of the message to be replied to, including bold, italic, underline, strikethrough, spoiler, and custom_emoji entities. The message will fail to send if the quote isn't found in the original message.
@@ -31,6 +30,9 @@ class ReplyParameters(Type_):
     :param quote_position: Optional. Position of the quote in the original message in UTF-16 code units
     :type quote_position: :obj:`int`
 
+    :param checklist_task_id: Optional. Identifier of the specific checklist task to be replied to
+    :type checklist_task_id: :obj:`int`
+
     :return: Instance of the class
     :rtype: :class:`ReplyParameters`
     """
@@ -44,6 +46,7 @@ class ReplyParameters(Type_):
         quote_parse_mode: "str" = None,
         quote_entities: List["tgram.types.MessageEntity"] = None,
         quote_position: "int" = None,
+        checklist_task_id: "int" = None,
         me: "tgram.TgBot" = None,
         json: "dict" = None,
     ):
@@ -55,6 +58,7 @@ class ReplyParameters(Type_):
         self.quote_parse_mode = quote_parse_mode
         self.quote_entities = quote_entities
         self.quote_position = quote_position
+        self.checklist_task_id = checklist_task_id
 
     @staticmethod
     def _parse(
@@ -76,6 +80,7 @@ class ReplyParameters(Type_):
                 if d.get("quote_entities")
                 else None,
                 quote_position=d.get("quote_position"),
+                checklist_task_id=d.get("checklist_task_id"),
             )
             if d and (force or me and __class__.__name__ not in me._custom_types)
             else None
